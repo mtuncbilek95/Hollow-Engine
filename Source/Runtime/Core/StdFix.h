3aaee93851cf217dfb86453cc8d0afd5feaba2cf
Metehan Tuncbilek <mtuncbilek95@gmail.com>
2024-06-24 22:50:15 +0300

    name change for math vectors

--- Source/Runtime/Core/StdFix.h
+++ Source/Runtime/Core/StdFix.h
@@ -11,6 +11,9 @@ using DArray = std::vector<T>;
 template<typename K, typename V>
 using Map = std::map<K, V>;
 
+template<typename K, typename V>
+using Pair = std::pair<K, V>;
+
 #include <set>
 template<typename T>
 using Set = std::set<T>;
@@ -43,37 +46,22 @@ inline OwnedPtr<T> MakeOwned(Args&&...args)
 	return std::make_unique<T>(std::forward<Args>(args)...);
 }
 
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-using Vector2f = glm::vec2;
-using Vector3f = glm::vec3;
-using Vector4f = glm::vec4;
 
-using Matrix4f = glm::mat4;
-using Matrix3f = glm::mat3;
+using Vec2f = glm::vec2;
+using Vec3f = glm::vec3;
+using Vec4f = glm::vec4;
 
-using Vector2i = glm::ivec2;
-using Vector3i = glm::ivec3;
-using Vector4i = glm::ivec4;
+using Mat4f = glm::mat4;
+using Mat3f = glm::mat3;
 
-using Vector2u = glm::uvec2;
-using Vector3u = glm::uvec3;
-using Vector4u = glm::uvec4;
+using Vec2i = glm::ivec2;
+using Vec3i = glm::ivec3;
+using Vec4i = glm::ivec4;
 
-using Vec2f = Vector2f;
-using Vec3f = Vector3f;
-using Vec4f = Vector4f;
-
-using Mat4f = Matrix4f;
-using Mat3f = Matrix3f;
-
-using Vec2i = Vector2i;
-using Vec3i = Vector3i;
-using Vec4i = Vector4i;
-
-using Vec2u = Vector2u;
-using Vec3u = Vector3u;
-using Vec4u = Vector4u;
+using Vec2u = glm::uvec2;
+using Vec3u = glm::uvec3;
+using Vec4u = glm::uvec4;
 
 namespace Math = glm;
